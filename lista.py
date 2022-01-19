422de8761f9dc3a575c4037fb1f2775f775da8d7
Francisco Lopez Gonzalez <95217464+franciscolopez13@users.noreply.github.com>
2022-01-19 18:52:25 +0100

    Update lista.py

--- lista.py
+++ lista.py
@@ -1,1 +1,33 @@
+def RANGO(valor, minimo, maximo):
+  try:
+    if valor >= minimo and valor <= maximo:
+      return True
+    else:
+      return False
+    except IndentationError:
+      return False 
 
+def estaEnLista(valor, lista):
+  try:
+    if valor in lista:
+      return True
+     else:
+      return False
+     except IndentationError:
+      return False
+
+min=1
+max=20
+variable=int(input(f"introduce un valor entre {min} y {max}: "))
+lista=[6,14,11,3,2,1,15,19]
+
+if RANGO(variable, min, max):
+  try:        
+    if estaEnLista(variable, lista):
+      print(f"el numero {variable} está dentro de la lista")
+    else:
+      print(f"el numero {variable} está en el rango pero no está en la lista")
+   except IndentationError:
+    print("esto no ha salido según lo esperado")
+else:
+  print(f"en número {variable} no está entre {min} y {max}" )
