a722834da6ec1832b5459eb42cf9969ba353b18f
Francisco Lopez Gonzalez <95217464+franciscolopez13@users.noreply.github.com>
2022-01-19 19:02:03 +0100

    Update binary.py

--- binary.py
+++ binary.py
@@ -1,7 +1,7 @@
 def Binario (strbinario):
     try:
-        for elemento in range(0,len(strbinario)):
-            if strbinario[elemento]!='1' and strbinario[elemento]!='0':
+        for numero in range(0,len(strbinario)):
+            if strbinario[numero]!='1' and strbinario[numero]!='0':
                 return False
         return True
     except TypeError:
@@ -10,9 +10,9 @@ def Binario (strbinario):
 def extraer (binario):
     try:
         decimal=0
-        for elemento in range(0,len(binario)):
-            if binario[elemento]=='1':
-                decimal=decimal+pow(2, len(binario)-elemento-1)
+        for numero in range(0,len(binario)):
+            if binario[numero]=='1':
+                decimal=decimal+pow(2, len(binario)-numero-1)
         return decimal
     except TypeError:
         return False
